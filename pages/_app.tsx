d52ec78871ec36510a24cc69e79235a7481de359
DAN001\SD073437 <aliye.kurumlu-vankerckhove@sdworx.com>
2022-08-04 18:31:30 +0200

    initial project with tailwinf typescript nextJS

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { AppProps } from "next/app";
+import "tailwindcss/tailwind.css";
+import Footer from "../components/footer";
+import Navigation from "../components/navigation";
+import { ThemeProvider } from "next-themes";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <ThemeProvider attribute="class" enableSystem={false}></ThemeProvider>
+      <Navigation />
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
